7a44faee70d9c4365412f706cf7e1df21052b7bf
chikwandagames <chikwandagames@gmail.com>
2021-03-08 17:24:29 +0000

    my first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/chikwandagames/bookings/pkg/config"
+	"github.com/chikwandagames/bookings/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// Here we user the bmizerany pat external package for routing
+
+func routes(app *config.AppConfig) http.Handler {
+	// Create a multiplexer, which is an http handler
+	mux := chi.NewRouter()
+	// Middlewere
+	// Middleware lets you process a request as it comes into the application
+	// and performes some action on it
+
+	// Help application, Gracefully absorb panics and prints the stack trace
+	mux.Use(middleware.Recoverer)
+	// Custom middleware
+	// mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/chikwandagames/bookings/pkg/config"
+	"github.com/chikwandagames/bookings/pkg/models"
+	"github.com/chikwandagames/bookings/pkg/render"
+)
+
+// Repo is the repository used by handlers
+// Here we use a repository pattern, to help us swap components
+// in and out off the application with minimal changes to the code.
+// We want the handlers to have access to config
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers set the repository for the handler
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home page, Handler function
+// In order for a function to respond to requests from a web browser
+// it has to handle 2 params, ResponseWriter and Request
+// Home is ...
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	// Get user IP and store in session
+	remoteIP := r.RemoteAddr
+	// Takes a context, key and value of item to store in session
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+
+}
+
+// About is the handler for the about page
+// Adding the receiver, gives About handler access to everything inside
+// the repository
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("about handler called")
+	// render.RenderTemplate(w, "about.page.html", &models.TemplateData{})
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello there"
+
+	// Get value stored in session
+	// Use GetString(), that way no need for casting
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
+}
