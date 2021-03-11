9b68fb5de179931c0fda9fe3ebb7d4a75243ee26
chikwandagames <chikwandagames@gmail.com>
2021-03-11 10:43:30 +0000

    custom js modules, update

--- js/main.js
+++ js/main.js
@@ -48,27 +48,65 @@ function notifyModal(title, text, icon, confirmButtonText) {
 
 document.getElementById('colorButton').addEventListener('click', () => {
   // notifyAlert("This is a message", "success")
-  notifyModal('Title', 'Hello', 'success', 'All good!')
-  // attention.toast()
+  // notifyModal('Title', 'Hello', 'success', 'All good!')
+  // attention.toast({ title: 'Hello world', icon: 'error' })
+  attention.error({ title: 'Oops!' })
   // console.log(myModule()["success"]())
 })
 
 const attention = myModule()
 
-// myModule encloses other functions, toast, success... 
+// myModule encloses other functions, toast, success...
 // if we imagine toast or success were thousands of lines of code
 // now we can access these using the var attention above
 function myModule() {
-  const toast = () => {
-    console.log('Clicked button and got toast')
+  const toast = (c) => {
+    // c will be overwritten by the const {} below, i.e. if is c is not specified
+    // we use the default values below
+    const { title = '', icon = 'success', position = 'top-end' } = c
+
+    const Toast = Swal.mixin({
+      toast: true,
+      // using the values above
+      title,
+      position,
+      icon,
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      },
+    })
+    // Run
+    Toast.fire({})
   }
 
-  const success = () => {
-    console.log('Clicked button and got success')
+  const success = (c) => {
+    const { title = '', icon = 'success', text = '', footer = '' } = c
+    Swal.fire({
+      icon,
+      title,
+      text,
+      footer,
+    })
   }
+
+  const error = (c) => {
+    const { title = '', icon = 'error', text = '', footer = '' } = c
+    Swal.fire({
+      icon,
+      title,
+      text,
+      footer,
+    })
+  }
+
 
   return {
     toast,
     success,
+    error
   }
 }
